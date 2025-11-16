9bd9a77ac445e00e5510b1d2d0acc60e8fcad1de
Shimon Darshan <Shimon.Darshan@gmail.com>
2025-11-16 21:15:15 +0200

    add: webhook provider specifc build
    Signed-off-by: Shimon Darshan <Shimon.Darshan@gmail.com>

--- pkg/register/webhook.go
+++ pkg/register/webhook.go
@@ -1,3 +1,5 @@
+//go:build webhook || all_providers
+
 /*
 Copyright © 2025 ESO Maintainer Team
 
@@ -19,12 +21,10 @@ package register
 
 import (
 	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
-	fake "github.com/external-secrets/external-secrets/providers/v1/fake"
-	kubernetes "github.com/external-secrets/external-secrets/providers/v1/kubernetes"
+	webhook "github.com/external-secrets/external-secrets/providers/v1/webhook"
 )
 
 func init() {
-	// Register Kuberntetes providers
-	esv1.Register(fake.NewProvider(), fake.ProviderSpec(), fake.MaintenanceStatus())
-	esv1.Register(kubernetes.NewProvider(), kubernetes.ProviderSpec(), kubernetes.MaintenanceStatus())
+	// Register webhook provider
+	esv1.Register(webhook.NewProvider(), webhook.ProviderSpec(), webhook.MaintenanceStatus())
 }
